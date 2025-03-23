85f87769b1d8767c73eb128265747f5d68a42b20
leonardo Henriques <leonardodacruzmateus@gmail.com>
2025-03-23 17:31:46 +0100

    remise a zèro du github

---
+++ frontend/src/app/components/historique/chart.js
@@ -0,0 +1,9 @@
+import Chart from 'chart.js/auto';
+
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.moveTo(50,150)
+    context.lineTo(250,50)
+    context.stroke();
+};
