d4cdc7856e6976bfda62a5ac3bf3cf3b6a75644f
LeonardoHenriquesM <leonardodacruzmateus@gmail.com>
2025-03-23 16:29:10 +0100

    Delete src directory

--- src/app/components/historique/chart.js
+++
@@ -1,9 +0,0 @@
-import Chart from 'chart.js/auto';
-
-window.onload = function() {
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
-    context.moveTo(50,150)
-    context.lineTo(250,50)
-    context.stroke();
-};
