ca6ef6f8e17e63e9ee63520c2b24604fa57164af
leonardo Henriques <leonardodacruzmateus@gmail.com>
2025-03-24 18:43:49 +0100

    Modification insciption.php

--- backend/api/src/inscription/inscription.php
+++ backend/api/src/inscription/inscription.php
@@ -1,5 +1,5 @@
 <?php
-require "../config/connexion_db.php"; // Assure-toi que tu as configuré la connexion à la base de données
+require "../config/connexion_db.php";
 
 header("Content-Type: application/json; charset=UTF-8");
 
@@ -22,14 +22,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $date_naissance = $data['date_naissance'];
     $date_inscription = $data['date_inscription'];
 
-    // Préparer la requête d'insertion
+    // Préparation de l'insertion
     $sql = "INSERT INTO eleves (Nom, Prenom, Email, password, Ville, Date_Naissance, Date_Inscription) 
             VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    // Préparer la requête
+    // Préparation la requête
     $stmt = $pdo->prepare($sql);
 
-    // Exécuter la requête
+    // Exécution de la requête
     if ($stmt->execute([$nom, $prenom, $email, $password, $ville, $date_naissance, $date_inscription])) {
         echo json_encode(["message" => "Utilisateur inséré avec succès"]);
     } else {
