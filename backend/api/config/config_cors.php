8400cafc0c5af67b163b72a96eb93b7729ea2b3b
Leo <leonardodacruzmateus@gmail.com>
2025-03-25 15:54:55 +0100

    modification fichier de config cors

--- backend/api/config/config_cors.php
+++ backend/api/config/config_cors.php
@@ -1,8 +1,9 @@
 <?php
 // Headers CORS et JSON
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: http://localhost:4200"); // Remplacez * par l'origine spécifique
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=utf-8");
 
 // Gérer la requête OPTIONS (préflight)
