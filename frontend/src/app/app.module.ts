a6e4073c6993550dd7ff706cfe58218141ca23d3
Nicraaw <nicragames9@gmail.com>
2025-03-25 23:28:05 +0100

    Messageeeee

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
@@ -13,8 +15,6 @@ import { ProfilComponent } from './components/profil/profil.component';
 import { SupportComponent } from './components/support/support.component';
 import { AvisComponent } from './components/avis/avis.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { ContactComponent } from './components/contact/contact.component';
 import { RdvComponent } from './components/rdv/rdv.component';
 import { StatsComponent } from './components/stats/stats.component'; 
--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
+  // Définir un objet pour stocker les valeurs du formulaire
+  formData = {
+    name: '',
+    object: '',  // Nouveau champ pour l'objet de la demande
+    message: ''
+  };
+
+  // Méthode qui est appelée lorsque le formulaire est soumis
+  onSubmit() {
+    const { name, object, message } = this.formData;
+
+    // Définir l'objet et le corps du message
+    const subject = `Demande de contact - ${object}`;
+    const body = `Nom: ${name}\nObjet: ${object}\n\nMessage:\n${message}`;
+
+    // Créer l'URL mailto
+    const mailtoLink = `mailto:nicolas.buisset@hotmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Ouvrir le client de messagerie avec le mailto
+    window.location.href = mailtoLink;
+  }
 }
